14c3c45b402f9ec499608787d84946d605c75e4b
Randolf Beerwerth <r.beerwerth@fz-juelich.de>
2020-05-28 20:50:13 +0200

    wip

--- Core/Computation/SpecularComputationTerm.cpp
+++ Core/Computation/SpecularComputationTerm.cpp
@@ -84,8 +84,8 @@ double SpecularMatrixTerm::intensity(const SpecularSimulationElement& elem,
 
     std::cout << "denom = " << denominator << std::endl;
 
-    std::cout << "rpp = " << M(2, 1) * M(1, 0) - M(2, 0) * M(1, 1) << std::endl;
-    std::cout << "rmm = " << M(3, 0) * M(0, 1) - M(3, 1) * M(0, 0) << std::endl;
+    std::cout << "  rpp = " << M(2, 1) * M(1, 0) - M(2, 0) * M(1, 1) << std::endl;
+    std::cout << "  rmm = " << M(3, 0) * M(0, 1) - M(3, 1) * M(0, 0) << std::endl;
 
     Eigen::Matrix2cd R;
     R(0, 0) = M(2, 1) * M(1, 0) - M(2, 0) * M(1, 1);
@@ -95,7 +95,12 @@ double SpecularMatrixTerm::intensity(const SpecularSimulationElement& elem,
 
     R /= denominator;
 
-    const complex_t result = (polarization * R.adjoint() * analyzer * R).trace();
+//    std::cout << "R = " << R << std::endl;
 
+//    std::cout << "pol = " << polarization << std::endl;
+//    std::cout << "analyzer = " << analyzer << std::endl;
+
+    const complex_t result = (polarization * R.adjoint() * analyzer * R).trace();
+//    std::cout << "ret = " << std::abs(result) << std::endl;
     return std::abs(result);
 }
