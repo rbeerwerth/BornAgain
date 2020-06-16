6ef0cf30dc61fdb26a58aa1456157dba80ae20a1
Randolf Beerwerth <r.beerwerth@fz-juelich.de>
2020-06-16 16:57:46 +0200

    first version of Nevot-Croce roughness for polarized strategy

--- Core/Multilayer/SpecularStrategyBuilder.cpp
+++ Core/Multilayer/SpecularStrategyBuilder.cpp
@@ -25,8 +25,8 @@ std::unique_ptr<ISpecularStrategy> SpecularStrategyBuilder::build(const MultiLay
     auto roughnessModel = sample.roughnessModel();
 
     if (magnetic) {
-        if (MultiLayerUtils::hasRoughness(sample))
-            throw std::runtime_error("Magnetic roughness not implemented");
+//        if (MultiLayerUtils::hasRoughness(sample))
+//            throw std::runtime_error("Magnetic roughness not implemented");
 
         return std::make_unique<SpecularMagneticNewStrategy>();
 //        return std::make_unique<SpecularMagneticStrategy>();
